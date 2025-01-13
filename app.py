a04b2a1f42d8ed144966caa8c7f223023281ab10
MustafaMstf <lwmus76@gmail.com>
2025-01-14 00:29:34 +0700

    Add all project files

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 from sklearn.model_selection import train_test_split
+import plotly.express as px
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
 
